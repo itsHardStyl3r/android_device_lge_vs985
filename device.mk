edb216cb662a38efaf29d75735dbf805392e918e
itsHardStyl3r <johnnylittleplanet@gmail.com>
2018-09-15 14:45:29 +0200

    vs985: TWRP update
     * building kernel inline
     * clean device tree
     * set selinux to permissive
     * alphabetical order in BoardConfig.mk
     * update mkbootimg.mk
     * move bump script to separate folder and fix chmod
     * update partitions
     * wipe omni.dependencies
     * fix init scripts
    
    thanks to @dadi11 and @ppajda for support!

--- device.mk
+++
@@ -1,3 +0,0 @@
-PRODUCT_COPY_FILES += \
-    device/lge/vs985/kernel:kernel \
-    device/lge/vs985/dtb:dtb
--- omni_vs985.mk
+++ omni_vs985.mk
@@ -1,6 +1,15 @@
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-$(call inherit-product, device/lge/vs985/full_vs985.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from vs985 device
+$(call inherit-product, device/lge/vs985/device.mk)
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_DEVICE := vs985
 PRODUCT_NAME := omni_vs985
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-VS985
+PRODUCT_MANUFACTURER := LGE
