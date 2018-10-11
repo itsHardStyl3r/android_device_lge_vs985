75968898019d6755608a2117a6f6dc7e1f684ac6
itsHardStyl3r <johnnylittleplanet@gmail.com>
2018-10-12 00:58:34 +0200

    PotatoProject for vs985

--- potato_vs985.mk
+++ potato_vs985.mk
@@ -21,14 +21,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from vs985 device
 $(call inherit-product, device/lge/vs985/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common POSP stuff.
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := vs985
-PRODUCT_NAME := lineage_vs985
+PRODUCT_NAME := potato_vs985
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-VS985
 PRODUCT_MANUFACTURER := LGE
+
+# Maintainer Prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="HardStyl3r"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
