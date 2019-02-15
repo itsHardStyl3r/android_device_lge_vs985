17d6173ca901ab25acb2b6f3570f9eab98a34cb0
Aleksander Gencel <johnnylittleplanet@gmail.com>
2019-02-15 14:17:10 +0100

    Bootleggers: Switch the bootanimation
      * 9th bootanimation does not scale on 1440p

--- bootleg_vs985.mk
+++ bootleg_vs985.mk
@@ -23,7 +23,7 @@ $(call inherit-product, device/lge/vs985/device.mk)
 # Inherit some common Bootleggers stuff.
 $(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
-TARGET_PICK_BOOTANIMATION := 9
+TARGET_PICK_BOOTANIMATION := 4
 
 PRODUCT_DEVICE := vs985
 PRODUCT_NAME := bootleg_vs985
