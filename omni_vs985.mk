64d2c83b60cc3b91e11fc6ac5d59ff9bbf82bc33
Aleksander Gencel <johnnylittleplanet@gmail.com>
2019-04-17 21:33:42 +0200

    vs985: Remove device.mk inheritance

--- omni_vs985.mk
+++ omni_vs985.mk
@@ -4,9 +4,6 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from vs985 device
-$(call inherit-product, device/lge/vs985/device.mk)
-
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := vs985
 PRODUCT_NAME := omni_vs985
