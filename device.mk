3f6cd9f6381c08d63cacd28f0a02134bb35c52dc
Aleksander Gencel <johnnylittleplanet@gmail.com>
2024-03-02 14:40:54 +0100

    Revert "vs985: Declare support for Mifare tags"
     * Commonized
    
    This reverts commit 501743f4ccab0bddff5a8c4cfeb56e13fadcba17.

--- device.mk
+++ device.mk
@@ -24,8 +24,7 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
